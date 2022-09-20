2fe920c2420c670853754cf91dffa3ae900f39e8
JustinLung <justinlung77@gmail.com>
2022-09-20 12:15:10 +0200

    introduction text made

---
+++ src/lib/components/Introduction.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let introductionTitle: string
+  export let introduction: string
+</script>
+
+<section>
+  <h2>{introductionTitle}</h2>
+  <p>{introduction}</p>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
   import Hero from '$lib/components/Hero.svelte'
+  import Introduction from '$lib/components/Introduction.svelte'
 </script>
 
 <Hero pageTitle="Your Tribe For Life" />
+<Introduction
+  introductionTitle="Your Tribe for life..."
+  introduction="Je hebt al een basis social network voor jouw squad. Breid dit social network uit naar een platform waarop de tribe members elkaar kunnen vinden en ondersteunen. In ieder geval gedurende het begin van jullie carriere als frontend designer & developer.
+De focus ligt op team building, samenwerken en hoe je frameworks inzet om een gezamenlijk doel te bereiken én in contact te blijven."
+/>
