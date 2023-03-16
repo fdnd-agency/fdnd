7a5f4c83a90f525bb567063066be6f304159c2f8
boudewijnbout <boudewijn_bout@outlook.com>
2023-03-16 16:22:25 +0100

    Removed .vercel folder

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const file = '_app/immutable/entry/_layout.svelte.34d55844.js';
-export const imports = ["_app/immutable/entry/_layout.svelte.34d55844.js","_app/immutable/chunks/index.7ec50c75.js","_app/immutable/chunks/paths.7e28016d.js"];
-export const stylesheets = ["_app/immutable/assets/_layout.31eb1a9f.css"];
-export const fonts = [];
--- .vercel/output/static/_app/immutable/chunks/1.e0609e11.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.a6b306bd.js";export{t as component};
