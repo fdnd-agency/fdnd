d0214740c00f9f2f7592e72c0b3b177c333bff6c
yassir bghiel <yassir.bghiel@hva.nl>
2025-02-10 16:02:14 +0100

    feat: made components for SVG, fixes #77 ✨
    fixed #77
    
    - Made components for each SVG --> cleaner code

--- src/lib/molecules/Pagination.svelte
+++ src/lib/molecules/Pagination.svelte
@@ -1,5 +1,7 @@
 <script>
   let { prevSprint, nextSprint } = $props();
+  import ArrowLeft from "../atoms/ArrowLeft.svelte";
+  import ArrowRight from "../atoms/ArrowRight.svelte";
 </script>
 
 <nav>
@@ -8,23 +10,8 @@
       data-sveltekit-reload
       href="/{prevSprint.semester.slug}/{prevSprint.slug}"
     >
-      <svg
-        aria-hidden="true"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
-          d="M5 12l14 0"
-        /><path d="M5 12l4 4" /><path d="M5 12l4 -4" /></svg
-      >
+      <ArrowLeft />
+      
       Vorige sprint
       <span>: {prevSprint.title}</span>
     </a>
@@ -37,23 +24,7 @@
     >
       Volgende sprint<span>: {nextSprint.title}</span>
 
-      <svg
-        aria-hidden="true"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
-          d="M5 12l14 0"
-        /><path d="M15 16l4 -4" /><path d="M15 8l4 4" /></svg
-      >
+      <ArrowRight />
     </a>
   {/if}
 </nav>
@@ -97,10 +68,6 @@
 
     a:last-of-type {
       margin-left: auto;
-    }
-
-    svg {
-      translate: 0 1px;
     }
   }
 </style>
